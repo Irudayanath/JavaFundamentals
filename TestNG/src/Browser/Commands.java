69598c7e1cbaf3708a48ff4fcd62e019bf6d30ac
admin <admin@admin-PC>
2015-07-07 00:48:35 +0530

    Change-Id: I2e07c315a1501cae10938bfd9cb52c97e4d347f2

--- TestNG/src/Browser/Commands.java
+++ TestNG/src/Browser/Commands.java
@@ -1,6 +1,7 @@
 package Browser;
 
 import java.io.File;
+
 import java.io.IOException;
 import java.util.Iterator;
 import java.util.Set;
@@ -8,14 +9,21 @@ import java.util.Set;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.Proxy;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import com.opera.core.systems.OperaSettings.Capability;
 
 public class Commands {
 
@@ -57,11 +65,50 @@ select.deselectByIndex(2);
 select.deselectAll();
 
 WebElement e=driver.findElement(By.xpath(""));
-Actions acrion=new Actions(driver);
-Actions mousemoveover=acrion.click();
-acrion.moveToElement(e);
+Actions a=new Actions(driver);
+Actions mousemoveover=a.click();
+a.moveToElement(e);
+a.dragAndDrop(source, target);
+Actions ad=new Actions(driver);
+Actions mouse=ad.click();
+ad.doubleClick();
+DesiredCapabilities dp=DesiredCapabilities.chrome();
+dp.setVersion("4");
+String p="User Agemt";
+String proxy="http:8080";
+Proxy p1=new Proxy();
+p1.setHttpProxy(proxy).setFtpProxy(proxy).setHttpsProxy(proxy);
+dp.setCapability(CapabilityType.PROXY,p1);
+WebDriver driver11=new ChromeDriver(dp);
+dp.setPlatform(Platform.WINDOWS);
+dp.setBrowserName("Chrome");
+dp.getPlatform();
+dp.getVersion();
 
 
+String proxy11="http:8080";
+String useragent="UserAgent";
+
+Proxy p11=new Proxy();
+p11.setHttpProxy(proxy11).setHttpsProxy(proxy11);
+
+dp.setCapability(CapabilityType.PROXY,p11);
+WebDriver driver111=new ChromeDriver(dp);
+
+
+driver.getWindowHandles();
+
+Set<String> sw=driver.getWindowHandles();
+Iterator st=sw.iterator();
+while(st.hasNext())
+{
+	String sa=it.next();
+	driver.switchTo().window(sa);
+	
+	
+}
+
+driver.switchTo().frame(driver.findElement(By.xpath("")));
 
 }
 }
