836ab8fd32fc6ff3d4ee1ef769870fa3689179b2
admin <admin@admin-PC>
2015-07-06 19:27:22 +0530

    Basics java programs

--- TestNG/src/CheckBox.java
+++ TestNG/src/CheckBox.java
@@ -1,29 +1,47 @@
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.FluentWait;
-
-import com.google.common.base.Function;
-import com.thoughtworks.selenium.Wait;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeTest;
 
 
 public class CheckBox {
 
 	
 	WebDriver driver;
-	@org.testng.annotations.Test
-	public void Test()
+	@BeforeTest
+	public void Capabilities()
 	{
+		DesiredCapabilities dp=new DesiredCapabilities();
+		dp.setBrowserName("Chrome");
+		dp.getPlatform();
+		dp.getBrowserName();
+		dp.getVersion();
+			
+		
+	}
+	@org.testng.annotations.Test
+	public void CheckBoxTestCase()
+	{
+
 		System.setProperty("webdriver.chrome.driver","C:/Selenium/chromedriver.exe");
 		driver=new ChromeDriver();
 		driver.get("http://www.echoecho.com/htmlforms09.htm");
+		WebDriverWait wait=new WebDriverWait(driver,10);
+		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@type='checkbox']")));
 		List<WebElement> checkbx=driver.findElements(By.xpath("//input[@type='checkbox']"));
 		System.out.println(checkbx.size());
+		 
+		driver.get("http://cssmenumaker.com/css-menus");
+		//Select s=new Select(driver.findElement(By.xpath("//select[@name='items_per_page']")));
+		//s.selectByVisibleText("24");
 		for(WebElement check:checkbx)
 		{
 			while(check.isDisplayed())
@@ -40,5 +58,4 @@ public class CheckBox {
 			
 	}
 	
-	}
-}
+	}}
--- TestNG/src/RadioButton.java
+++ TestNG/src/RadioButton.java
@@ -14,12 +14,44 @@ public void radiocheck()
 	WebDriver driver;
 	
 	System.setProperty("webdriver.chrome.driver","C:/Selenium/chromedriver.exe");
-	for(int i=0;i<10;i++)
-	{
+	
 		
 	
 	driver=new ChromeDriver();
+	List<WebElement> elements=driver.findElements(By.tagName("a"));
+	List<WebElement> radioButtin=driver.findElements(By.xpath("//input[@type='radio']"));
+	List<WebElement> CheckBox=driver.findElements(By.xpath("//input[@type='checkbox']"));
+	String linktext[]=new String[elements.size()];
+	int i=0;
+	for(WebElement e:elements)
+	{
+		linktext[i]=e.getText();
+		i++;
+	}
+	for(String s:linktext)
+	{
+		driver.findElement(By.linkText(s));
+		driver.getTitle().equalsIgnoreCase("ddd");
+		
+	}
 	
+	for(WebElement e:radiobuttin)
+	{
+		if(e.isSelected())
+	
+		{
+			
+			
+		}
+
+		}
+	
+	for(WebElement e:CheckBox)
+	{
+		if(e.isSelected())
+		{
+			
+		}
 	/**driver.get("http://www.echoecho.com/htmlforms10.htm");
 	List<WebElement> radiochk=driver.findElements(By.xpath("//input[@type='radio']"));
 	System.out.println(radiochk.size());
