47080cdf808e17a6e5e6594a97d50a40396ab438
admin <admin@admin-PC>
2015-07-07 00:12:54 +0530

    my first change
    Change-Id: I9413e8ae815a7be048fa2d75b43cfffc0261a21d

---
+++ Java Basics/src/Arraylist.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+
+
+public class Arraylist {
+
+	public static void main(String args[])
+	{
+		ArrayList l=new ArrayList();
+		l.add(23);
+		l.add(45);
+		l.add(4533);
+		l.add(1);
+		System.out.println(l);
+		Iterator it=l.iterator();
+		while(it.hasNext())
+		{
+			Object s=it.next();
+			System.out.println(s);
+			
+		}
+		Collections.sort(l);
+		System.out.println(l);
+	}
+}
